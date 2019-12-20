00d26af2ece4333f7e8028583a5ffd7bb8dbca34
양원석 <wonseok.yang@thingsflow.kr>
2019-12-20 19:22:36 +0900

    initialize

---
+++ PointFreeFunctions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PointFreeFunctions
+//
+//  Created by 양원석 on 2019/12/20.
+//  Copyright © 2019 양원석. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PointFreeFunctionsTests/PointFreeFunctionsTests.swift
@@ -0,0 +1,34 @@
+//
+//  PointFreeFunctionsTests.swift
+//  PointFreeFunctionsTests
+//
+//  Created by 양원석 on 2019/12/20.
+//  Copyright © 2019 양원석. All rights reserved.
+//
+
+import XCTest
+@testable import PointFreeFunctions
+
+class PointFreeFunctionsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
